c8a446bd5ff52cbdaf2f9765162b6dde974afa82
Christer Brannstrom <christer.brannstrom@gmail.com>
2012-05-21 19:29:40 +0100

    Added logging controller to verify connection strings

---
+++ Molibar.Mini/Controllers/LoggingController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Configuration;
+using System.Web.Management;
+using System.Web.Mvc;
+
+namespace Molibar.Mini.Controllers
+{
+    public class LoggingController : Controller
+    {
+        public ActionResult Index()
+        {
+            var sqlServerConnectionString =
+                ConfigurationManager.AppSettings["SQLSERVER_CONNECTION_STRING"];
+            var sqlServerUri =
+                ConfigurationManager.AppSettings["SQLSERVER_URI"];
+            var sqlServerConnectionStringAlias =
+                ConfigurationManager.AppSettings["SQLSERVER_CONNECTION_STRING_ALIAS"];
+            var message = string.Format("CONSTR {0}, SERVERURI {1}, ALIAS {2}",
+                          sqlServerConnectionString,
+                          sqlServerUri,
+                          sqlServerConnectionStringAlias);
+            new LogEvent(message).Raise();
+            return new EmptyResult();
+        }
+    }
+
+    public class LogEvent : WebRequestErrorEvent
+    {
+        public LogEvent(string message)
+            : base(null, null, 100001, new Exception(message))
+        {
+        }
+    }
+}
